1b121acaa19266736726aa4aead3fde17a80b105
dgkf <18220321+dgkf@users.noreply.github.com>
2022-04-15 12:38:54 -0700

    minor update to test to verify input data

--- tests/testthat/test-rd2markdown-list.R
+++ tests/testthat/test-rd2markdown-list.R
@@ -1,9 +1,14 @@
 test_that("rd2markdown formats LIST Rd_tags", {
-  list_rd <- rawRd("
-    \\describe{
-      This is a {test} of LIST Rd_tags.
-    }
-  ")
+  list_rd <- rawRd("\\describe{This is a {test} of LIST Rd_tags.}")
 
-  expect_match(rd2markdown(list_rd), "test of LIST Rd_tags")
+  expect_silent({
+    which_describe <- lapply(list_rd, attr, "Rd_tag") == "\\describe"
+    describe <- list_rd[which_describe][[1L]]
+  })
+
+  # confirm that we actually produced a LIST Rd_tag as part of our test
+  expect_true(any(lapply(describe, attr, "Rd_tag") == "LIST"))
+
+  # test rendering of LIST element, {test}
+  expect_match(rd2markdown(list_rd), "is a test of")
 })
