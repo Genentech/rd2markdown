8c6c6c2672c687db84acc0fed21ad8b47a3d1fdb
Szymon Maksymiuk <32574056+maksymiuks@users.noreply.github.com>
2024-10-23 18:39:12 +0200

    Refine how macros are discovered (#37)
    * Refine how macros are discovered
    
    * Remove leftovers
    
    * Add discovery tests

--- tests/testthat/test-get_rd.R
+++ tests/testthat/test-get_rd.R
@@ -10,10 +10,21 @@ test_that("Can find relevant R documentation files, given a .Rd filepath", {
   expect_s3_class(get_rd(file = rd_filepaths[[1L]]), "Rd")
 })
 
-test_that("get_rd find macros properly", {
+test_that("get_rd find macros by file", {
   rd_filepath <- system.file("examples", "rd_file_sample_2.Rd", package = "rd2markdown")
-  expect_s3_class(get_rd(file = rd_filepath, macros = NA), "Rd")
-  expect_silent(rd <- get_rd(file = rd_filepath, macros = NA))
+  rd_macro <- system.file("examples", "macros", "macros.Rd", package = "rd2markdown")
+  expect_s3_class(get_rd(file = rd_filepath, macros = rd_macro), "Rd")
+  expect_silent(rd <- get_rd(file = rd_filepath, macros = rd_macro))
+  expect_equivalent(
+    rd[[17]][[3]],
+    structure(c("\\code{\\link[=#1]{#1()}}", "rnorm"), Rd_tag = "USERMACRO", macro = "\\function")
+  )
+})
+
+test_that("get_rd find macros embedded in the package", {
+  rd_filepath <- system.file("examples", "rd_file_sample_2.Rd", package = "rd2markdown")
+  expect_s3_class(get_rd(file = rd_filepath, macros = NULL), "Rd")
+  expect_silent(rd <- get_rd(file = rd_filepath, macros = NULL))
   expect_equivalent(
     rd[[17]][[3]],
     structure(c("\\code{\\link[=#1]{#1()}}", "rnorm"), Rd_tag = "USERMACRO", macro = "\\function")
